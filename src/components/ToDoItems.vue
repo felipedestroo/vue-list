509564e9a0b443bb15c701ca2980df75f8da8c44
felipedestroo <felipedestrodev@gmail.com>
2026-07-26 15:54:45 -0300

    feat: add ToDo components for task management

---
+++ src/components/ToDoItems.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <ToDoItem 
+            v-for="todo in $store.state.todos"
+            :key="todo.id"
+            :todo="todo"
+        />
+    </div>
+</template>
+
+<script>
+import ToDoItem from './ToDoItem.vue';
+export default {
+    components: { ToDoItem }
+}
+</script>
